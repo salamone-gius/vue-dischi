0eef4742eff0003f6616d673a7b8f239a576dd61
salamone-gius <salamone.gius@gmail.com>
2022-06-07 17:36:21 +0200

    axios installed and request to server done

--- src/components/main/AlbumsSection.vue
+++ src/components/main/AlbumsSection.vue
@@ -1,15 +1,35 @@
 <template>
-  <section>
-
+  <section class="albums">
+    <div class="container">
+      <div class="row">
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'AlbumsSection'
+  name: 'AlbumsSection',
+  created() {
+    // Make a request for a user with a given ID
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then((response) => {
+      // handle success
+      console.log(response.data.response);
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    })
+    .then(() => {
+      // always executed
+    });
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 
 </style>
