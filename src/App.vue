a507ed3dddaca4a4c628f73667c227974795c4bb
salamone-gius <salamone.gius@gmail.com>
2022-06-07 16:46:18 +0200

    scaffolding created

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
+    <BaseHeader/>
+    <BaseMain/>
   </div>
 </template>
 
 <script>
+import BaseHeader from './components/header/BaseHeader.vue';
+import BaseMain from './components/main/BaseMain.vue';
 
 export default {
   name: 'App',
   components: {
-    
+    BaseHeader,
+    BaseMain,
   }
 }
 </script>
 
 <style lang="scss">
 @import './assets/style/global.scss';
+
+* {
+  border: 1px solid red;
+}
 
 </style>
--- src/components/header/BaseHeader.vue
+++ src/components/header/BaseHeader.vue
@@ -1,5 +1,9 @@
 <template>
-  
+  <header>
+       <div class="container">
+           <img src="" alt="Spotify-logo">
+       </div>
+  </header>
 </template>
 
 <script>
@@ -9,5 +13,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+header {
+    background-color: var(--primary-color);
+}
 </style>
