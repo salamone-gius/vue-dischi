1182b0430d568593d1f108a7c4cb497c6c7524db
salamone-gius <salamone.gius@gmail.com>
2022-06-07 16:55:08 +0200

    BaseHeader component created

--- src/components/header/BaseHeader.vue
+++ src/components/header/BaseHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <header>
-       <div class="container">
-           <img src="" alt="Spotify-logo">
-       </div>
+    <div class="container">
+        <img src="../../assets/img/spotify-logo.png" alt="Spotify-logo">
+    </div>
   </header>
 </template>
 
@@ -15,5 +15,9 @@ export default {
 <style lang="scss" scoped>
 header {
     background-color: var(--primary-color);
+
+    img{
+        max-width: 3.75rem;
+    }
 }
 </style>
